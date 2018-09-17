35a4ed8e7b112682000e4d27aa680c18393a2fc8
andihoerudin24 <andihoerudin24@gmail.com>
2018-09-17 10:31:00 -0700

    show detail post

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Post;
+use DB;
 class BlogController extends Controller
 {
     public function index()
@@ -12,5 +13,12 @@ class BlogController extends Controller
 
       $posts=Post::with('author')->orderBy('created_at','desc')->simplePaginate(3);
       return view('blog.index',compact('posts'))->render();
+    }
+
+    public function show($id)
+    {
+        $post=Post::findOrFail($id);
+        //$post=DB::table('posts')->where('id',$id)->first();
+        return view('blog.show',compact('post'));
     }
 }
