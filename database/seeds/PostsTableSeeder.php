c46bbbdf97c1f92297e2b6c0a1f6140489612090
andihoerudin24 <andihoerudin24@gmail.com>
2018-09-17 06:48:47 -0700

    update seed and update publish post or blog

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Faker\Factory;
+use Carbon\Carbon;
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -13,11 +14,15 @@ class PostsTableSeeder extends Seeder
     {
         DB::table('posts')->truncate();
         $faker=Factory::create();
+        $date=Carbon::create(2016,7,18,9);
         $posts=[];
         for($i=1; $i<=10; $i++)
         {
             $image="Post_Image_".rand(1,5) . ".jpg";
             //$date=date("Y-m-d H:i:s",strtotime("2019-07-18 08:00:00 + {$i}days"));
+            $date->addDays(1);
+            $publishedDate=clone($date);
+            $createdDate=clone($date);
             $posts[]=[
                 'author_id'=>rand(1,3),
                 'title'=>$faker->sentence(rand(8,12)),
@@ -25,8 +30,9 @@ class PostsTableSeeder extends Seeder
                 'body'=>$faker->paragraphs(rand(10,15), true),
                 'slug'=>$faker->slug(),
                 'image' =>rand(0,1) == 1 ? $image : null,
-                'created_at'=>new DateTime,
-                'updated_at'=>new DateTime
+                'created_at'=>$createdDate,
+                'updated_at'=>$createdDate,
+                'published_at'=>$i < 5 ? $publishedDate : (rand(0,1)== 0 ? NULL : $publishedDate->addDays(4))
             ];
         }
           DB::table('posts')->insert($posts);
